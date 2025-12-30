37c1b37a2c0cb2dee2add161b0bcdab13b584b02
jeffthecool9 <107611378+jeffthecool9@users.noreply.github.com>
2025-12-30 18:07:01 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js'
+import App from 'src/App.jsx'
 import './index.css' // remove this line if you don’t have index.css
 
 ReactDOM.createRoot(document.getElementById('root')).render(
